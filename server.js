fd478fe12ca18396f95a48ff565bb6baed33c4c9
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-05-30 17:37:12 -0400

    small fixes

--- server.js
+++ server.js
@@ -2,18 +2,18 @@
 //dependencies
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require("method-override");
+var exphbs = require('express-handlebars');
+var db = require("./models");
+//Heroku dynamically assigns the port. Need to fetch from env. 3000 port works locally
+var PORT = process.env.PORT || 3000;
 /*The extended option allows to choose between parsing the URL-encoded data with the querystring library
 (when false) or the qs library (when true). The "extended" syntax allows for rich objects and arrays to be encoded
 into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.*/
-var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//Heroku dynamically assigns the port. Need to fetch from env. 3000 port works locally
-var PORT = process.env.PORT || 3000;
-var methodOverride = require("method-override");
-var exphbs = require('express-handlebars');
-app.use(bodyParser.urlencoded({ extended: false }));
-var db = require("./models");	
+app.use(bodyParser.urlencoded({ extended: false }));	
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 // Set Handlebars.
